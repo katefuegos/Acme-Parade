1f7ee7df4f2bb7ff9b4e0428bd9f549be9b8616a
Ferkaj <f_javi_fm@hotmail.com>
2019-03-28 19:05:01 +0100

    Fix test request

--- Acme-Parade/src/main/java/services/RequestService.java
+++ Acme-Parade/src/main/java/services/RequestService.java
@@ -1,5 +1,7 @@
+
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,6 +13,7 @@ import org.springframework.util.Assert;
 
 import repositories.RequestRepository;
 import security.LoginService;
+import domain.Parade;
 import domain.Request;
 
 @Service
@@ -20,12 +23,16 @@ public class RequestService {
 	// Repository-----------------------------------------------
 
 	@Autowired
-	private RequestRepository requestRepository;
+	private RequestRepository	requestRepository;
 
 	// Services-------------------------------------------------
 
 	@Autowired
-	private MemberService memberService;
+	private MemberService		memberService;
+
+	@Autowired
+	private BrotherhoodService	brotherhoodService;
+
 
 	// Constructor----------------------------------------------
 
@@ -38,8 +45,7 @@ public class RequestService {
 	public Request create() {
 		final Request res = new Request();
 		res.setStatus("PENDING");
-		res.setMember(memberService.findByUserAccountId(LoginService
-				.getPrincipal().getId()));
+		res.setMember(this.memberService.findByUserAccountId(LoginService.getPrincipal().getId()));
 		return res;
 	}
 
@@ -54,18 +60,11 @@ public class RequestService {
 
 	public Request save(final Request request) {
 		Assert.notNull(request);
-		if (request.getStatus().equals("REJECTED")) {
-			Assert.notNull(request.getReasonReject(),
-					"SI UN REQUEST ES RECHAZADO DEBE DE DECIRSE LA RAZÓN");
-		}
+		if (request.getStatus().equals("REJECTED"))
+			Assert.notNull(request.getReasonReject(), "SI UN REQUEST ES RECHAZADO DEBE DE DECIRSE LA RAZÓN");
 		if (request.getStatus().equals("ACCEPTED")) {
-			Assert.isTrue(request.getRoow() != null
-					&& request.getColuumn() != null,
-					"SI SE ACEPTA UN REQUEST SE DEBE INTRODUCIR ROOW Y COLUUMN");
-			Assert.isTrue(
-					this.findRequestByPosition(request.getRoow(), request
-							.getColuumn(), request.getParade().getId()) == null,
-					"ESTA POSICION YA ESTÁ OCUPADA EN ESA PROCESION");
+			Assert.isTrue(request.getRoow() != null && request.getColuumn() != null, "SI SE ACEPTA UN REQUEST SE DEBE INTRODUCIR ROOW Y COLUUMN");
+			Assert.isTrue(this.findRequestByPosition(request.getRoow(), request.getColuumn(), request.getParade().getId()) == null, "ESTA POSICION YA ESTÁ OCUPADA EN ESA PROCESION");
 		}
 		final Request saved = this.requestRepository.save(request);
 		return saved;
@@ -76,18 +75,46 @@ public class RequestService {
 	}
 
 	// Other Methods--------------------------------------------
-	public Request findRequestByPosition(int roow, int coluumn, int paradeId) {
-		return requestRepository.findRequestByPosition(roow, coluumn,
-				paradeId);
+	public void flush() {
+		this.requestRepository.flush();
 	}
 
-	public Collection<Request> findRequestByParadeId(int paradeId) {
+	public Request application(final Request request) {
+
+		final domain.Member member = this.memberService.findByUserAccountId(LoginService.getPrincipal().getId());
+		Assert.notNull(member);
+		Assert.isTrue(request.getParade().getStatus().equals("ACCEPTED"));
+		final Collection<Request> requests = this.findRequestByMemberId(member.getId());
+		final Collection<Parade> parades = new ArrayList<>();
+		if (!requests.isEmpty())
+			for (final Request r : requests)
+				parades.add(r.getParade());
+		Assert.isTrue(!parades.contains(request.getParade()));
+
+		return this.requestRepository.save(request);
+	}
+
+	public Request reject(final Request request) {
+		Assert.isTrue(request != null);
+		Assert.isTrue(request.getParade().getBrotherhood().getId() == this.brotherhoodService.findByUserAccountId(LoginService.getPrincipal().getId()).getId());
+		Assert.isTrue(!request.getReasonReject().equals(""));
+		Assert.isTrue(request.getStatus().equals("PENDING"));
+
+		request.setStatus("REJECTED");
+
+		return this.requestRepository.save(request);
+	}
+	public Request findRequestByPosition(final int roow, final int coluumn, final int paradeId) {
+		return this.requestRepository.findRequestByPosition(roow, coluumn, paradeId);
+	}
+
+	public Collection<Request> findRequestByParadeId(final int paradeId) {
 		Assert.notNull(paradeId);
-		return requestRepository.findRequestByParadeId(paradeId);
+		return this.requestRepository.findRequestByParadeId(paradeId);
 	}
 
-	public Collection<Request> findRequestByMemberId(int memberId) {
+	public Collection<Request> findRequestByMemberId(final int memberId) {
 		Assert.notNull(memberId);
-		return requestRepository.findRequestByMemberId(memberId);
+		return this.requestRepository.findRequestByMemberId(memberId);
 	}
 }
